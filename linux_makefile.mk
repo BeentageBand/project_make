adcedca6e08aed0922aafd3a167676022e8aea20
BeentageBand <josera.baez@gmail.com>
2017-07-31 20:22:47 -0500

    done

--- linux_makefile.mk
+++ linux_makefile.mk
@@ -1,13 +1,13 @@
 define LINUX_MAKE
 CC:=gcc
-CFLAGS:=-std=gnu99 -Wall -pthread -g
+CFLAGS:=-std=c99 -Wall -pthread -g
 AR:=ar
 AFLAGS:=-rcs
 LFLAGS:=-rcT
 CP:=-cp
 CPFLAGS:=-sf
 CPP:=g++
-CPPFLAGS:=-std=gnu++11 -Wall -pthread -g
+CPPFLAGS:=-std=c++11 -Wall -pthread -g
 endef
 
 $(eval $(call Verbose,$(call LINUX_MAKE)))
